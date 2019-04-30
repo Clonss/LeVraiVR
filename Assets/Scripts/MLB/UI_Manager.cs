d2638051aa2a8c4ed3912ebc3d4f30bb98ca9d1c
Zetsubo9 <max.lebolloch@laposte.net>
2019-04-30 14:40:12 +0200

    Affichage Stats

---
+++ Assets/Scripts/MLB/UI_Manager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_Manager : MonoBehaviour
+{
+    public GameObject statsScreen;
+    public Text timerText;
+    public float objectif;
+    private float timerValue = 0f;
+
+    public static UI_Manager s_Singleton;
+
+
+    private void Awake()
+    {
+        if(s_Singleton != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            s_Singleton = this;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(timerValue < objectif)
+        {
+            timerValue += Time.deltaTime;
+            UpdateTimerDisplay();
+        }
+        if(timerValue >= objectif)
+        {
+            DisplayStatsScreen();
+        }
+    }
+    public void DisplayStatsScreen()
+    {
+        statsScreen.SetActive(true);
+    }
+    public void UpdateTimerDisplay()
+    {
+        timerText.text = timerValue.ToString("00:00");
+    }
+}
