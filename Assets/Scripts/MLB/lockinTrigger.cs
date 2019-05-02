daaf3b272dd21417edb6296c6d03c76b2a59edb3
Zetsubo9 <max.lebolloch@laposte.net>
2019-05-02 16:18:04 +0200

    progression MLB

---
+++ Assets/Scripts/MLB/lockinTrigger.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class lockinTrigger : MonoBehaviour
+{
+    public GameObject triggerObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("triggerObject"))
+        {
+            transform.Rotate(1, 2, 3);
+        }
+    }
+}
