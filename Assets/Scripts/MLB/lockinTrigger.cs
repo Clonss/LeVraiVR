45db06d3893c24a2b90c5aee3bf26ba0807dfadf
Zetsubo9 <max.lebolloch@laposte.net>
2019-05-09 21:06:19 +0200

    Lock de l'objet

--- Assets/Scripts/MLB/lockinTrigger.cs
+++ Assets/Scripts/MLB/lockinTrigger.cs
@@ -5,23 +5,26 @@ using Valve.VR.InteractionSystem;
 
 public class lockinTrigger : MonoBehaviour
 {
-    public GameObject triggerObject;
+    public Transform lockForm;
     // Start is called before the first frame update
     void Start()
     {
-        
+        lockForm = GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        
     }
 
     public void OnTriggerEnter(Collider other)
     {
+        lockForm = GetComponent<Transform>();
         if (other.CompareTag("triggerObject"))
         {
-            transform.Rotate(1, 2, 3);
+            lockForm.eulerAngles = new Vector3(0, 0, 0);
+            gameObject.GetComponent<Rigidbody>().freezeRotation = true;
         }
     }
 }
