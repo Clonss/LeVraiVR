668b428ccb116a9c78b2589e31bb73175a405251
Clonss <gerardmaxime1@outlook.fr>
2019-05-16 14:59:18 +0200

    Enigme de la cuisine commencee

---
+++ Assets/Scripts/EnigmeCuisine.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnigmeCuisine : MonoBehaviour
+{
+    public GameObject ingr1;
+    public GameObject ingr2;
+    public GameObject ingr3;
+
+    public bool ready;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        /*if(other.gameObject == ingr1 && other.gameObject == ingr2 && other.gameObject == ingr3)
+        {
+            Debug.Log("oui");
+        }*/
+
+        Debug.Log(other);
+    }
+}
