202416d5b829751680062fb4c5c1337215f463a2
Clonss <gerardmaxime1@outlook.fr>
2019-05-17 16:23:32 +0200

    Script Horloge completement fonctionnels

---
+++ Assets/Scripts/EssaiMinute.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EssaiMinute : MonoBehaviour
+{
+    private Vector3 minutes;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        minutes = Vector3.zero;
+        minutes.x = 3f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.Rotate(minutes * Time.deltaTime);
+    }
+}
