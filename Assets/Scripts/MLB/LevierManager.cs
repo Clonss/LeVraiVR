4eadb25958c396b0d2c9e0362824590904698caa
Zetsubo9 <max.lebolloch@laposte.net>
2019-05-21 09:02:07 +0200

    scene fondu

---
+++ Assets/Scripts/MLB/LevierManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevierManager : MonoBehaviour
+{
+    public GameObject fondu;
+    public float timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(fondu.active == true)
+        {
+            timer += Time.deltaTime;
+        }
+        if (timer >= 2)
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+
+    public void OnleverDown()
+    {
+        fondu.SetActive(true);
+    }
+}
