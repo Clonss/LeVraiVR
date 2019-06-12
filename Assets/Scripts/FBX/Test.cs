1a74c2e24aee29b0ccdaab2b909f5f4fb48bfc2f
Lune <ilfautquejemensouvienne@gmail.com>
2019-06-12 15:36:04 +0200

    MAIS LES VFX ET LE SON DU GRAB QUOI

---
+++ Assets/Scripts/FBX/Test.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject target;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        Vector3 dir = (target.transform.position - this.transform.position).normalized;
+        this.transform.position = dir * Time.deltaTime;
+    }
+}
