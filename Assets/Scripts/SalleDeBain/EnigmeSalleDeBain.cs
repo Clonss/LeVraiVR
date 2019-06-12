1aad694434d1bb562b24cc876538b3700a6ed718
Clonss <gerardmaxime1@outlook.fr>
2019-06-12 15:15:04 +0200

    MAIS LA SALLE DE BAIN QUOI

--- Assets/Scripts/SalleDeBain/EnigmeSalleDeBain.cs
+++ Assets/Scripts/SalleDeBain/EnigmeSalleDeBain.cs
@@ -4,16 +4,32 @@ using UnityEngine;
 
 public class EnigmeSalleDeBain : MonoBehaviour
 {
+    public int count;
+
+    private bool validated;
+
+    private GameManager gameManager;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        count = 0;
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Check();
+    }
+
+    void Check()
+    {
+        if (count >= 5 && validated == false)
+        {
+            gameManager.ghostsNbr++;
+            count = 0;
+            validated = true;
+        }
     }
 }
