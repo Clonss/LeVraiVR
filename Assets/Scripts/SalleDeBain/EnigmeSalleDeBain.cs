e278bd865f14db818a153b93415132eb0873239a
Clonss <gerardmaxime1@outlook.fr>
2019-06-12 11:20:12 +0200

    Sah kel plésire

--- Assets/Scripts/SalleDeBain/EnigmeSalleDeBain.cs
+++ Assets/Scripts/SalleDeBain/EnigmeSalleDeBain.cs
@@ -6,10 +6,13 @@ public class EnigmeSalleDeBain : MonoBehaviour
 {
     private GameObject oui1;
     private GameObject oui2;
+    public GameObject firstObject;
+    public GameObject secondObject;
+
     // Start is called before the first frame update
     void Start()
     {
-        oui1 = null;
+        oui1 = gameObject;
         oui2.name = "oui2";
     }
 
@@ -21,15 +24,16 @@ public class EnigmeSalleDeBain : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if(oui1.name != "Obj3")
+        
+        if (oui1.name != firstObject.name)
         {
             oui1 = other.gameObject;
             Debug.Log(oui1.name);
         }
-        /*else if(oui2.name != "Obj4")
+        if(oui2.name != secondObject)
         {
             oui2 = other.gameObject;
             Debug.Log(oui2.name);
-        }*/
+        }
     }
 }
---
+++ Assets/Scripts/SalleDeBain/SavonSDB.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SavonSDB : MonoBehaviour
+{
+    private GameObject triggerFirstObject;
+    private GameObject triggerSecondObject;
+    public GameObject firstObject;
+    public GameObject secondObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        triggerFirstObject = gameObject;
+        triggerSecondObject = gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (triggerFirstObject.name != firstObject.name)
+        {
+            triggerFirstObject = other.gameObject;
+            Debug.Log(triggerFirstObject.name);
+        }
+
+        if(secondObject != null)
+        {
+            if (triggerSecondObject.name != secondObject.name)
+            {
+                triggerSecondObject = other.gameObject;
+                Debug.Log(triggerSecondObject.name);
+            }
+        }
+        
+    }
+}
