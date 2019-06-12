6231339b5ace36c10e200e530a0b3c5f469f9cca
Clonss <gerardmaxime1@outlook.fr>
2019-06-12 11:35:29 +0200

    et voila

--- Assets/Scripts/SalleDeBain/EnigmeSalleDeBain.cs
+++ Assets/Scripts/SalleDeBain/EnigmeSalleDeBain.cs
@@ -4,36 +4,16 @@ using UnityEngine;
 
 public class EnigmeSalleDeBain : MonoBehaviour
 {
-    private GameObject oui1;
-    private GameObject oui2;
-    public GameObject firstObject;
-    public GameObject secondObject;
 
     // Start is called before the first frame update
     void Start()
     {
-        oui1 = gameObject;
-        oui2.name = "oui2";
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        
-        if (oui1.name != firstObject.name)
-        {
-            oui1 = other.gameObject;
-            Debug.Log(oui1.name);
-        }
-        if(oui2.name != secondObject)
-        {
-            oui2 = other.gameObject;
-            Debug.Log(oui2.name);
-        }
     }
 }
